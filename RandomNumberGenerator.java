19f397313b256698751d57b8cb8b2b7e8eb6b2c8
Bernard <bernard.oconnor47@mail.dcu.ie>
2016-04-11 13:25:33 +0100

    Finish the RandomNumberGenerator

--- RandomNumberGenerator.java
+++ RandomNumberGenerator.java
@@ -4,51 +4,102 @@ import java.util.*;
 public class RandomNumberGenerator {
 	public static void main(String [] args) {
 		//Variables
-		String answer;
 		boolean increasing = false;
+		int min;
+		int max;
 		int size;
 		//Code
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Do you want numbers to be in ascending order? y/n");
-		answer = scan.nextLine();
+		if(scan.nextLine().equals("y")){
+			increasing = true;
+		}
+		System.out.println("Input min and max values, with max > 0 (min max)");
+		min = scan.nextInt();
+		max = scan.nextInt();
 		System.out.println("Input the size of the array:");
 		size = scan.nextInt();
 		
-		if(answer.equals("y")) {
-			increasing = true;
-		} 
 		
-		generateNumber(increasing, size);
+		generateList(increasing, size, min, max);
 	}
 	
-	static void swap(int[] array, int a, int b) {
-		int tmp = array[a];
-		array[a]=array[b];
-		array[b]=tmp;
+	static int generateRandomInt(int min, int max){
+		return min + (int)(Math.random() * ((max - min) + 1));
+	}
+
+	static void swap(ArrayList<Integer> arrayList, int a, int b) {
+		int tmp = arrayList.get(a);
+		arrayList.set(a, arrayList.get(b));
+		arrayList.set(b, tmp);
 	}
 	
-	static void generateNumber(boolean increasing, int size) {
-		int [] num = new int[size];
+	static void addAdditionalElements(ArrayList<Integer> arrayList , int inputtedSize)  
+	{
+		int countOfValidElements = 0;
+		int highestValidElementSoFar = 0;
+		
+		//count how many elements valid elements we have
+
+		for(int i = 0; i < arrayList.size(); i++)
+		{
+			if(arrayList.get(i) > highestValidElementSoFar)
+			{
+				
+				highestValidElementSoFar = arrayList.get(i);
+				countOfValidElements++;
+			}
+			
+		}
+
+		
+		//calculate how many elements need to be added
+		int additionalSize = inputtedSize - countOfValidElements;
+
+
+		if(highestValidElementSoFar <= 0) //make sure that the highestValidElementSoFar is indeed a valid element (incase no values in the list were valid)
+		{
+			highestValidElementSoFar = 1;
+		}
+
+		//start adding additional elements.  Note we may go over the max value here, but the max value is an arbitrary maximum
+		for(int j = 0; j < additionalSize; j++){
+			highestValidElementSoFar++;	//add a new highest element to the end of the list
+			arrayList.add(highestValidElementSoFar);
+		}
+
+
+	}
+
+	static void generateList(boolean increasing, int size, int min, int max) {
+		ArrayList<Integer> num = new ArrayList<>();
+		int randomNum;
 		
 		for(int i = 0; i < size; i++) {
-			num[i] = (int)(Math.random() * 65535) + 1;
+			randomNum = generateRandomInt(min, max);
+			num.add(randomNum);
 		}
 		
 		if(increasing) {
 			for(int i = 1; i < size-1; i++){
 				for(int j = 0; j < size-i; j++){
-					if(num[j] > num[j+1]){
+					if(num.get(j) > num.get(j+1)){
 						swap(num, j, j+1);
 					}
 				}
 			}
 		}
+
+		
+		addAdditionalElements(num, size);  
+		
 		
 		try {
 			File output = new File("output.txt");
 			PrintWriter printer = new PrintWriter(output);
-			for(int i = 0; i < size; i++) {
-				printer.write("" + num[i]+ "\n");
+			printer.write(size + "\n");  //The first number must be the size of the array
+			for(int i = 0; i < num.size(); i++) {
+				printer.write("" + num.get(i)+ "\n");
 			}
 			printer.flush();
 			printer.close();
