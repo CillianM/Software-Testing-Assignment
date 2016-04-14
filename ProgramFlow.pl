e618c37f72316f01527cd968591ffd745140250d
Shaun Kinsella <shaun.kinsella38@mail.dcu.ie>
2016-04-14 14:20:14 +0100

    Update for atom names + comments
    Changed atom names to reflect program purpose

--- ProgramFlow.pl
+++ ProgramFlow.pl
@@ -1,6 +1,3 @@
-
-
-
 /**
     *ProgramFlow pathfinder
     *Fact list of interconnected nodes.
@@ -12,54 +9,54 @@
 
 
  
-road(start, "1", 1).
-road("1", "2", 1).
-road("2", "3", 1).
-road("3", "4", 1).
-road("4", "5", 1).
-%road("4", "19", 1). %comment this one out to generate 2nd shortest route
-road("4Loop", "19", 1).
-road("5", "6", 1).
-road("5", "9", 1).
-road("6", "7", 1).
-road("7", "8", 1).
-road("8", "7Loop", 1).
-road("7Loop", "4Loop", 1).
-road("9", "10", 1). 
-road("10", "11", 1).
-road("11", "10Loop", 1).
-road("10", "12", 1).
-road("10Loop", "12", 1). 
-road("12", "18", 1).
-road("18", "4Loop", 1).
-road("12", "13", 1).
-road("13", "14", 1).
-road("14", "15", 1).
-road("15", "16", 1).
-road("16", "4Loop", 1).
-road("15", "17", 1).
-road("17", "4Loop", 1).  
-road("19", finish, 1).
+path(start, "1", 1).
+path("1", "2", 1).
+path("2", "3", 1).
+path("3", "4", 1).
+path("4", "5", 1).
+%path("4", "19", 1). %comment this one out to generate 2nd shortest route
+path("4Loop", "19", 1).
+path("5", "6", 1).
+path("5", "9", 1).
+path("6", "7", 1).
+path("7", "8", 1).
+path("8", "7Loop", 1).
+path("7Loop", "4Loop", 1).
+path("9", "10", 1). 
+path("10", "11", 1).
+path("11", "10Loop", 1).
+path("10", "12", 1).
+path("10Loop", "12", 1). 
+path("12", "18", 1).
+path("18", "4Loop", 1).
+path("12", "13", 1).
+path("13", "14", 1).
+path("14", "15", 1).
+path("15", "16", 1).
+path("16", "4Loop", 1).
+path("15", "17", 1).
+path("17", "4Loop", 1).  
+path("19", finish, 1).
 
 
 %function route to calculate all possible routes from facts above
-route(StartTown, EndTown, Route, NDist) :-                              
-    route(StartTown, EndTown, [StartTown], 0, Route, NDist). 
+route(StartNode, EndNode, Route, NDist) :-                              
+    route(StartNode, EndNode, [StartNode], 0, Route, NDist). 
     
-route(StartTown, EndTown, Visited, DistAcc, Route, NDist) :-    
-    road(StartTown, EndTown, Dist),                                     
-    reverse([EndTown|Visited] , Route),                                               
+route(StartNode, EndNode, Visited, DistAcc, Route, NDist) :-    
+    path(StartNode, EndNode, Dist),                                     
+    reverse([EndNode|Visited] , Route),                                               
     NDist is DistAcc + Dist.                                
         
-route(StartTown, EndTown, Visited, DistAcc, Route, NDist) :-
-    road(StartTown, Midpoint, Dist),
+route(StartNode, EndNode, Visited, DistAcc, Route, NDist) :-
+    path(StartNode, Midpoint, Dist),
     not(member(Midpoint, Visited)),                                                         
     NewDistAcc is DistAcc + Dist,                                            
-    route(Midpoint, EndTown, [Midpoint|Visited], NewDistAcc, Route, NDist).  
+    route(Midpoint, EndNode, [Midpoint|Visited], NewDistAcc, Route, NDist).  
 
 %returns shortest route from list of ordered routes.    
-shortest(StartTown, EndTown, Route, NDist) :-
-    setof((NDist, Route), route(StartTown, EndTown, Route, NDist), [(NDist, Route)|_]).    
+shortest(StartNode, EndNode, Route, NDist) :-
+    setof((NDist, Route), route(StartNode, EndNode, Route, NDist), [(NDist, Route)|_]).    
     
 
 
@@ -67,8 +64,6 @@ shortest(StartTown, EndTown, Route, NDist) :-
 /**Usage as follows "route(start, finish, R, N)."
     Where R = Route taken
           N = Node cost
-
-
     R = [start, "1", "2", "3", "4", "5", "6", "7", "8", "7Loop", "4Loop", "19", finish],
     N = 12 ;
     R = [start, "1", "2", "3", "4", "5", "9", "10", "11", "10Loop", "12", "18", "4Loop", "19", finish],
@@ -83,16 +78,11 @@ shortest(StartTown, EndTown, Route, NDist) :-
     N = 15 ;
     R = [start, "1", "2", "3", "4", "5", "9", "10", "12", "13", "14", "15", "17", "4Loop", "19", finish],
     N = 15 ;
-
-
     [1] 12 ?- shortest(start, finish, R, N).
     R = [start, "1", "2", "3", "4", "19", finish],    This will only result if ladder of size 0 is entered is read in.
     N = 6.
-
     Actual shortest logical path
     [1] 14 ?- shortest(start, finish, R, N).
     R = [start, "1", "2", "3", "4", "5", "9", "10", "12", "18", "4Loop", "19", finish],
     N = 12.
-
-
 */
